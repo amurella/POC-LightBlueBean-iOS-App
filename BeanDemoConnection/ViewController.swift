72f1b8465fc5c0847dc81bd36cafa448f2cf29fe
amurella <amurella@Akhilas-MacBook-Pro.local>
2017-04-17 13:15:43 -0700

    Serial Data is Being Received

--- BeanDemoConnection/ViewController.swift
+++ BeanDemoConnection/ViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 import Bean_iOS_OSX_SDK
+import CoreBluetooth
 
 class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate {
 
+    @IBOutlet weak var valueFromBean: UILabel!
     @IBOutlet weak var ledTextLabel: UILabel!
     var beanManager: PTDBeanManager!
     var myBean: PTDBean!
     var lightState: Bool = false
+    var startReading: Bool = false
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -25,40 +29,6 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
         beanManager!.delegate = self
     }
     
-    /*override func viewDidAppear(_ animated: Bool)
-    {
-        // super.viewDidAppear(true)
-        /* let controller = UIAlertController(title: "Bluetooth Connection is in process", message: "This app requires a Bluetooth Connection", preferredStyle: .alert)
-         present(controller, animated:true, completion: nil)*/
-        startScanning()
-        
-        
-        /* dismissAlertClosure =
-         //            {
-         self.dismiss(animated: true, completion: nil)
-         let controller = UIAlertController(title: "Bluetooth Connection is successful!", message: "This app has created a Bluetooth Connection", preferredStyle: .alert)
-         present(controller, animated:true, completion: nil)
-         }*/
-        
-        //      var endTime: dispatch_time_t = DispatchTime.now(dispatch_time_t(DISPATCH_TIME_NOW), Int64(5.0 * Double(NSEC_PER_SEC)))
-        //   dispatch_after(endTime, DispatchQueue.main, {self.beanDiscovered = true})
-    }*/
-    
-    /*    func checkConnectionAnimation()
-     {
-     while(beanDiscovered == false)
-     {
-     
-     }
-     
-     if(beanDiscovered == true)
-     {
-     
-     self.dismiss(animated: true, completion: nil)
-     }
-     
-     }*/
-    
     
     func beanManagerDidUpdateState(_ beanManager: PTDBeanManager!)
     {
@@ -68,15 +38,6 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
             print("made it here")
             startScanning()
             print("made it here part 2")
-            
-            /*if let e = scanError
-             {
-             print(e)
-             }
-             else
-             {
-             print("Please turn on your Bluetooth")
-             }*/
         }
     }
     
@@ -93,40 +54,7 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
             print("Connection is made")
         }
     }
-    
-    /* func beanManager(_ beanManager: PTDBeanManager!, didDiscover bean: PTDBean!, error: Error!) {
-     print ("HELLO")
-     if let e = error
-     {
-     print(e)
-     }
-     print("Found a bean: \(bean.name)")
-     if myBean == nil
-     {
-     if bean.state == .discovered
-     {
-     if bean.name == "Bean"
-     {
-     myBean = bean
-     print("discovered to the bean")
-     connectToBean(bean: myBean!)
-     print("connected to the bean")
-     }
-     }
-     connectToBean(bean: myBean!)
-     }
-     }*/
-    
-    /*func beanManager(_ beanManager: PTDBeanManager!, didDiscover bean: PTDBean!, error: Error!) {
-     if let e = error {
-     print(e)
-     }
-     print("Found a Bean: \(bean.name)")
-     myBean = bean
-     myBean!.delegate = self
-     connectToBean(bean: myBean!)
-     
-     }*/
+
     
     func beanManager(_ beanManager: PTDBeanManager!, didDiscover bean: PTDBean!, error: Error!) {
         if myBean == nil {
@@ -136,6 +64,7 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
                 viewDidAppear(true)
                 myBean = bean
                 beanManager!.connect(to: bean, withOptions:nil, error: nil)
+                bean.delegate = self
             }
         }
         
@@ -144,41 +73,22 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
         #endif
     }
     
-    /*  func beanManager(_ beanManager: PTDBeanManager!, didDiscover bean: PTDBean!, error: NSError!) {
-     print("pls luv me")
-     
-     
-     
-     if bean.name == "Bean" {
-     
-     }
-     }*/
+ 
     
-    /* func beanManager(_ beanManager: PTDBeanManager!, didConnect bean: PTDBean!, error: Error!) {
-     if myBean == nil
-     {
-     myBean = bean
-     print("Officially connected")
-     }
-     }*/
     
-    //to receive bean code
-    /*  func beanManager(_ beanManager: PTDBeanManager!, didConnect bean: PTDBean!, error: Error!)
-     {
-     bean.delegate = self
-     }
+   
      
-     func bean(_ bean: PTDBean!, serialDataReceived data: Data!)
+    /* func bean(_ bean: PTDBean!, serialDataReceived data: Data!)
      {
      if(data != nil)
      {
-     var receivedMessage = NSString(data:data, encoding: NSUTF8StringEncoding)
+     var receivedMessage = NSString(data:data, encoding: String.Encoding.utf8.rawValue)
      print("From serial: (receivedMessage)")
      ledTextLabel.text = receivedMessage! as string
      }
      }*/
     
-    func connectToBean(bean: PTDBean)
+   /* func connectToBean(bean: PTDBean)
     {
         var error: NSError?
         print ("hi")
@@ -187,9 +97,16 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
         bean.delegate = self
         
         
+    }*/
+    
+    
+    @IBAction func pressValueToStartReading(_ sender: Any)
+    {
+        startReading = true
+        let data = NSData(bytes: &lightState, length: MemoryLayout<Bool>.size)
+        sendSerialData(beanState: data)
+        print ("HI")
     }
-    
-    
     func updateLedStatusText(lightState: Bool)
     {
         let onOffText = lightState ? "ON": "OFF"
@@ -201,17 +118,53 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
         print(myBean)
         print("bye")
         myBean?.sendSerialData(beanState as Data!)
+        print("sent over serial data")
     }
     
+
     func bean(_ bean: PTDBean!, serialDataReceived data: Data!)
     {
-        //put this in a loop (while serialDataReceived < 1000 (so led has been turned on) or stop has been pressed)
         if(data != nil)
         {
-            print(data)
-            var receivedMessage = NSString(data:data, encoding: String.Encoding.utf8.rawValue)
-            ledTextLabel.text = receivedMessage! as String
+            print ("Received Data")
         }
+        
+        var stringData : String = NSString(data: data, encoding : String.Encoding.ascii.rawValue) as! String
+        valueFromBean.text? += stringData
+        //var fullString : String = valueFromBean.text!
+        
+       // valueFromBean.scrollRangeToVisible(NSMakeRange(count(valueFromBean.string!),0))
+        //put this in a loop (while serialDataReceived < 1000 (so led has been turned on) or stop has been pressed)
+       /* if(data != nil)
+        {
+            var stringReceived: String = ""
+            
+            let nLength: Int = data.count / MemoryLayout<UInt8>.size
+            var arrData: [UInt8] = [UInt8](repeating: 0, count: nLength)
+            data.copyBytes(to: &arrData, count: nLength)
+            
+            var n: Int = 0
+            while(n < nLength)
+            {
+                let str = String(UnicodeScalar(arrData[n]))
+                stringReceived += str
+                n += 1
+            }
+            print(stringReceived)
+         //   var receivedMessage = NSString(data:data, encoding: String.Encoding.utf8.rawValue)
+            valueFromBean.text = stringReceived as String
+        }*/
+    
+        //    NSString *uuidSubstring = [bean.identifier.UUIDString substringFromIndex:bean.identifier.UUIDString.length-4];
+        //    serialOutput = [NSString stringWithFormat:@"%@-%@:%@", bean.name, uuidSubstring, serialOutput];
+        
+       /* if(serialOutput)
+        {
+            serialOutput = [self.consoleOutputTextView.string stringByAppendingString:serialOutput];
+            self.consoleOutputTextView.string = serialOutput;
+        }
+        
+        [self.consoleOutputTextView scrollRangeToVisible: NSMakeRange(self.consoleOutputTextView.string.length, 0)];*/
     }
     
     @IBAction func pressButtonToChangeValue(_ sender: Any)
